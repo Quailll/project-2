751b64b051a8ddc97a5955f13442dc821bd01ad2
Dyllan <dphamada92@gmail.com>
2023-04-03 13:46:59 -0700

    just testing and trying to figure out server problems

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const sequelize = require('./config/connection')
 const exphbs = require('express-handlebars')
-const helpers = require('./utils/helpers')
+// const helpers = require('./utils/helpers')
 
 
 const app = express();
@@ -10,17 +10,21 @@ const PORT = process.env.PORT || 3001;
 
 // const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+// app.engine("handlebars", hbs.engine);
+// app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
-Sequelize.sync({ force: false }).then(() =>{
 
-  app.listen(PORT, () => {
-    console.log(`Listening to port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`)
+  // Sequelize.sync({ force: false })
     
   });
+
+app.get('/', function (req, res) {
+  req.render('home');
+
 });
