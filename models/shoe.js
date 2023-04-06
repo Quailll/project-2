dfa6dcf9827673f5465175379a95b0ea7cf73c12
MaJo <mateuskingjoo@gmail.com>
2023-04-05 17:50:58 -0700

    updated controller & api

--- models/shoe.js
+++ models/shoe.js
@@ -5,6 +5,12 @@ class Shoe extends Model {}
 
 Shoe.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
