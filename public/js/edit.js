2a1e8ff7b6c0f42be0d7d1c76d73f8e1d29069c3
Dyllan <dphamada92@gmail.com>
2023-04-07 19:33:56 -0700

    added logic to edit

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,1 +1,29 @@
-const postEdit = document.querySelector()
+const shoeId = document.querySelector("input[name='shoe-id']").value;
+
+const editShoe = async (event) =>{
+  event.preventDefault(); 
+  await fetch(`/api/shoes/${shoeId}`,{
+    method: "PUT",
+    body: JSON.stringify({
+      name, 
+      brand, 
+      style, 
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+  document.location.replace("/home");
+};
+
+const deleteBtn = async () => {
+  await fetch(`/api/shoes/${shoeId}`, {
+    method: 'DELETE'
+  });
+  document.location.replace('/home')
+};
+
+
+
+document.querySelector("#name-edit").addEventListener("submit", editShoe);
+document.querySelector("#delbtn").addEventListener("click", deleteBtn);
